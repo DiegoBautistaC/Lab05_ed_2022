f9b9088a68003ed10a05de704accf539c1c7f472
DiegoBautista <da10bautista@gmail.com>
2022-04-18 15:24:27 -0600

    Adición librería de clases y referencia del proyecto

---
+++ Lab05_ed_2022/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab05_ed_2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
